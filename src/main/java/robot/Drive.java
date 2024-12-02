647276ebb4a9f83fac3d5b7c9298adeaf35d6041
Sgtryan10 <67563290+Sgtryan10@users.noreply.github.com>
2024-12-01 21:11:14 -0500

    Update Drive.java

--- src/main/java/robot/Drive.java
+++ src/main/java/robot/Drive.java
@@ -1,1 +1,28 @@
+package robot.drive;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Drive extends SubsystemBase {  
+  private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
+
+  public Drive() {
+      for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
+	      spark.restoreFactoryDefaults();
+	      spark.setIdleMode(IdleMode.kBrake);
+      }
+    
+    rightFollower.follow(rightLeader);
+    leftFollower.follow(leftLeader);
+
+    leftLeader.setInverted(true);
+  }
+
+  private void drive(double leftSpeed, double rightSpeed) {
+    leftLeader.set(leftSpeed);
+    rightLeader.set(rightSpeed);
+  }
+}
